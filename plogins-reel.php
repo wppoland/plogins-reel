4f5eac5d5a3cd5dcac61b1e8ca76537586254d09
wppoland <szatkowski.mariusz+github@gmail.com>
2026-07-28 15:15:20 +0200

    update storefront-kit to the version carrying the punctuation fix

--- plogins-reel.php
+++ plogins-reel.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Plogins Reel - Product Gallery Zoom & Video for WooCommerce
  * Plugin URI:        https://plogins.com/plogins-reel/
  * Description:        Hover zoom, lightbox and featured video for the WooCommerce product gallery: reserved space, no jQuery.
- * Version:           1.0.7
+ * Version:           1.0.8
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Requires Plugins:  woocommerce
@@ -25,7 +25,7 @@ namespace Reel;
 
 defined('ABSPATH') || exit;
 
-const VERSION     = '1.0.7';
+const VERSION     = '1.0.8';
 const PLUGIN_FILE = __FILE__;
 
 define('REEL_DIR', plugin_dir_path(__FILE__));
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: woocommerce, product gallery, product video, image zoom, lightbox
 Requires at least: 6.5
 Tested up to: 7.0
 Requires PHP: 8.1
-Stable tag: 1.0.7
+Stable tag: 1.0.8
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
